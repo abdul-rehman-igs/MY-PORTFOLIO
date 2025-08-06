1df003eb3b66c5de6d3c410b603e3a96fa110da6
Abdul Rehman <arkhalid2019@gmail.com>
2025-08-06 14:02:26 +0500

    My Portfolio Project

---
+++ send_mail.php
@@ -0,0 +1,26 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $to = "arkhalid2019@gmail.com";  // Your email
+    $subject = "New Contact Form Message";
+
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $phone = htmlspecialchars($_POST['phone']);
+    $message = htmlspecialchars($_POST['message']);
+
+    $body = "You received a new message from your website contact form:\n\n";
+    $body .= "Name: $name\n";
+    $body .= "Email: $email\n";
+    $body .= "Phone: $phone\n\n";
+    $body .= "Message:\n$message\n";
+
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message sent successfully.";
+    } else {
+        echo "Failed to send message.";
+    }
+}
+?>
