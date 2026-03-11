22684f569542976d1fe95fa7488b4891fb310cc0
Abdul Rehman <arkhalid2019@gmail.com>
2026-03-12 04:18:08 +0500

    Update typed.js strings for typing effect

--- assets/js/script.js
+++ assets/js/script.js
@@ -76,7 +76,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Web development", "", "Data Analyst", "Content Writing"],
+    strings: ["Data Engineer", "Data Analyst", "Web development"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
